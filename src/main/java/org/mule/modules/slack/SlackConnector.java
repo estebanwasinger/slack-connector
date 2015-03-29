2f5b9f03c5aeaa913627c706cfb9647fca83320b
estebanwasinger <estebanwasinger@gmail.com>
2015-03-29 12:39:44 -0300

    Added - retrieveMessages @Source method - send files as input stream - send messages with attachments

--- src/main/java/org/mule/modules/slack/SlackConnector.java
+++ src/main/java/org/mule/modules/slack/SlackConnector.java
@@ -5,29 +5,35 @@
 
 package org.mule.modules.slack;
 
-import org.mule.api.annotations.ConnectionStrategy;
-import org.mule.api.annotations.Connector;
-import org.mule.api.annotations.MetaDataScope;
-import org.mule.api.annotations.Processor;
+import org.apache.log4j.Logger;
+import org.mule.api.annotations.*;
 import org.mule.api.annotations.display.FriendlyName;
 import org.mule.api.annotations.display.Path;
 import org.mule.api.annotations.display.Summary;
 import org.mule.api.annotations.oauth.OAuthProtected;
+import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.MetaDataKeyParam;
 import org.mule.api.annotations.param.Optional;
+import org.mule.api.callback.SourceCallback;
+import org.mule.modules.slack.strategy.OAuth2ConnectionStrategy;
 import org.mule.modules.slack.strategy.SlackConnectionStrategy;
 import org.stevew.SlackClient;
+import org.stevew.exceptions.SlackException;
 import org.stevew.exceptions.UserNotFoundException;
 import org.stevew.model.User;
 import org.stevew.model.channel.Channel;
 import org.stevew.model.chat.Message;
 import org.stevew.model.chat.MessageResponse;
+import org.stevew.model.chat.attachment.ChatAttachment;
+import org.stevew.model.chat.attachment.Field;
 import org.stevew.model.file.FileUploadResponse;
 import org.stevew.model.group.Group;
 import org.stevew.model.im.DirectMessageChannel;
 import org.stevew.model.im.DirectMessageChannelCreationResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,6 +43,8 @@ import java.util.List;
  */
 @Connector(name = "slack", friendlyName = "Slack")
 public class SlackConnector {
+
+    private static final Logger logger = Logger.getLogger(SlackConnector.class);
 
     @ConnectionStrategy
     SlackConnectionStrategy connectionStrategy;
@@ -130,6 +138,16 @@ public class SlackConnector {
     @OAuthProtected
     @Processor
     @MetaDataScope(AllChannelCategory.class)
+    public MessageResponse postMessageWithAttachment(@Optional String message, @FriendlyName("Channel ID") @MetaDataKeyParam String channelId, @Optional @FriendlyName("Name to show") String username, @Optional @FriendlyName("Icon URL") String iconURL, @Default("#[payload]") ChatAttachment chatAttachment, Field field) {
+        List<Field> fields = new ArrayList<Field>();
+        fields.add(field);
+        chatAttachment.setFields(fields);
+        return slack().sendMessageWithAttachment(message, channelId, username, iconURL, chatAttachment);
+    }
+
+    @OAuthProtected
+    @Processor
+    @MetaDataScope(AllChannelCategory.class)
     public Boolean deleteMessage(@FriendlyName("Message TimeStamp") String timeStamp, @FriendlyName("Channel ID") @MetaDataKeyParam String channelId) {
         return slack().deleteMessage(timeStamp, channelId);
     }
@@ -201,8 +219,6 @@ public class SlackConnector {
     public Boolean archiveGroup(@MetaDataKeyParam String channelID){
         return slack().archiveGroup(channelID);
     }
-    
-    
 
     //***********
     // Files methods
@@ -215,22 +231,60 @@ public class SlackConnector {
         return slack().sendFile(channelID, fileName, fileType, title, initialComment, filePath);
     }
 
-    /*@OAuthProtected
+
+    @OAuthProtected
+    @Processor
+    @MetaDataScope(AllChannelCategory.class)
+    public FileUploadResponse uploadFileAsInputStreams(@Summary("Channel ID to send the message") @FriendlyName("Channel ID") @MetaDataKeyParam String channelID, @Summary("File name to show in the Slack message") @Optional String fileName,
+                                                       @Optional String fileType, @Summary("Message title")@Optional String title, @Optional String initialComment, @Summary("Input Stream Reference of where to look the file to upload") @Default("#[payload]") InputStream inputStream) throws IOException {
+        return slack().sendFile(channelID, fileName, fileType, title, initialComment, inputStream);
+    }
+
+    //************
+    // Source methods
+    //************
+
+
     @Source
-    public void retrieveMessages(SourceCallback source, Integer authSleep, Integer messageRetreiveInterval) throws Exception{
+    @MetaDataScope(AllChannelCategory.class)
+    public Message retrieveMessages(SourceCallback source, Integer messageRetreiveInterval, @MetaDataKeyParam @FriendlyName("Channel ID") String channelID) throws Exception{
+        String oldestTimeStamp = String.valueOf(new java.util.Date().getTime());
+
+        if(getConnectionStrategy().getClass().equals(OAuth2ConnectionStrategy.class)){
+            while (true){
+                logger.error("Retrieve Messages source doesn't work with OAuth 2 configuration, please use Connection Management");
+                Thread.sleep(5000);
+            }
+        }
 
         while(!getConnectionStrategy().isAuthorized()){
-            Thread.sleep(authSleep);
+            Thread.sleep(1000);
+            logger.debug("Waiting authorization!");
         }
+
+        logger.info("Started retrieving messages of channel: "+slack().getChannelById(channelID).getName() +"!");
+
+        oldestTimeStamp = slack().getChannelHistory(channelID,null,null,"1").get(0).getTs();
 
     	while (true)
         {	
             Thread.sleep(messageRetreiveInterval);
-            Message ms = new Message();
-            ms.setText(slack().isConnected().toString());
-            source.process(ms);
+            //System.out.println("Retrieving messages!");
+            List<Message> messages = slack().getChannelHistory(channelID, null, oldestTimeStamp, "1000");
+            //System.out.println("Oldest TS:" + oldestTimeStamp);
+            if(messages.size()==0){
+              //  System.out.println("No Updates!");
+            }else {
+                oldestTimeStamp = messages.get(0).getTs();
+            }
+            Integer i = messages.size();
+
+            while(i>0){
+                source.process(messages.get(i-1));
+                i--;
+            }
         }
-    }*/
+    }
 
     protected SlackClient slack() {
         return connectionStrategy.getSlackClient();
