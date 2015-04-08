1403d1d6cd6eacc50c00cb8660d89ac159bbee1b
Juan Desimoni <juan.desimoni@mulesoft.com>
2015-04-08 20:07:24 +0000

    Connector Documentation Deploy

---
+++ index.js
@@ -0,0 +1,20 @@
+$(".dropdown").on("click", function(e){
+	  e.preventDefault();
+
+	  if($(this).hasClass("open")) {
+	    $(this).removeClass("open");
+	    $(this).children("ul").slideUp("fast");
+	  } else {
+  		var list = document.getElementsByClassName("dropdown");;
+
+		for (var i=0, item; item = list[i]; i++) {
+		  	  if($(item).hasClass("open")) {
+			    $(item).removeClass("open");
+			    $(item).children("ul").slideUp("fast");
+			  }
+		}
+
+	    $(this).addClass("open");
+	    $(this).children("ul").slideDown("fast");
+	  }
+});
